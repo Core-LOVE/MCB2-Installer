ae2d83d44155f0a355e7d77594071bf570da60bd
katebulka <ekaterinabolakova0@gmail.om>
2026-05-16 22:34:55 +0400

    Fixing installation potentially

--- install_src/src/main/java/net/katebulka/installer/FinishInstallation.java
+++ install_src/src/main/java/net/katebulka/installer/FinishInstallation.java
@@ -1,7 +1,10 @@
 package net.katebulka.installer;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FinishInstallation {
     public static boolean deleteDirectory(File directory) {
@@ -16,6 +19,20 @@ public class FinishInstallation {
         return directory.delete();
     }
 
+    private static void copyContents(Path source, Path destination) throws IOException {
+        try (Stream<Path> walk = Files.walk(source)) {
+            for (Path sourcePath : walk.collect(Collectors.toList())) {
+                Path relativePath = source.relativize(sourcePath);
+                Path destPath = destination.resolve(relativePath);
+                if (Files.isDirectory(sourcePath)) {
+                    Files.createDirectories(destPath);
+                } else {
+                    Files.copy(sourcePath, destPath, StandardCopyOption.REPLACE_EXISTING);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         if (args.length > 0) {
             String gamepath = args[0];
@@ -27,6 +44,18 @@ public class FinishInstallation {
 
             Path from = Paths.get(gamepath + "/install_output");
             Path destination = Path.of(gamepath);
+
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(from)) {
+                for (Path sourceSubdir : stream) {
+                    if (Files.isDirectory(sourceSubdir)) {
+                        String folderName = sourceSubdir.getFileName().toString();
+                        Path targetDir = Path.of(gamepath, folderName);
+
+                        Files.createDirectories(targetDir);
+                        copyContents(sourceSubdir, targetDir);
+                    }
+                }
+            }
 
             try (DirectoryStream<Path> stream = Files.newDirectoryStream(from)) {
                 for (Path entry : stream) {
